4f0af0a1e404777fd215fdfbbfa194fbaba55e57
Deepika <deepika.gunda@gmail.com>
2020-08-10 22:43:46 +0530

    first commit

---
+++ src/components/ui/Theme.js
@@ -0,0 +1,44 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+/*const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+*/
+const arcBlue = "#0B7289";
+const arcOrange = "#FFBA60";
+export default createMuiTheme({
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+  typography: {
+    tab: {
+      fontFamily: "Raleway",
+      textTransform: "none",
+      fontWeight: 700,
+      fontSize: "1rem",
+    },
+    estimate: {
+      fontSize: "1rem",
+      fontFamily: "Pacifico",
+      color: "white",
+      textTransform: "none",
+    },
+  },
+});
